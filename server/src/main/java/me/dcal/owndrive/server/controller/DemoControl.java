df2ea93ecd6a0e203c2467391e11c5e533cf2412
berthout_martin <martin@berthout.nom.fr>
2020-05-05 16:14:51 +0200

    Demo service rest second commit

--- server/src/main/java/me/dcal/owndrive/server/controller/DemoControl.java
+++ server/src/main/java/me/dcal/owndrive/server/controller/DemoControl.java
@@ -4,12 +4,16 @@ import me.dcal.owndrive.server.model.DemoResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class DemoControl {
 
+    private AtomicLong counter = new AtomicLong();
+
     @GetMapping("/demo")
     public DemoResource getDemoResource() {
-
+        return new DemoResource(counter.incrementAndGet(), "");
     }
 
 }
