91d70a62b20b202e0ac700ac31eba98c04f51f22
mberthout <martin.berthout@gmail.com>
2020-06-15 18:10:56 +0200

    client c affichage doc en ligne + download

--- clients/clientC/client.c
+++ clients/clientC/client.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <curl/curl.h>
 #include <string.h>
 
 #define UPLOAD_URL "http://192.168.56.1:8080/file/savePublicFile/"
+#define DOWNLOAD_URL "http://192.168.56.1:8080/file/getPublicFile/"
+
+static size_t writeData(void *ptr, size_t size, size_t nmemb, void *stream) {
+  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+  return written;
+}
 
 int mainMenu() {
     int choice;
@@ -21,6 +28,15 @@ int mainMenu() {
     return choice;
 }
 
+void performCurlRequest(CURL* curl, char* curlOkMessage) {
+    CURLcode res = curl_easy_perform(curl);
+    if (CURLE_OK != res) {
+        fprintf(stderr, "Request failed: %s\n", curl_easy_strerror(res));
+    } else {
+        printf("%s", curlOkMessage);
+    }
+}
+
 void sendFile(char* path, char* fileName) {    
     // Init curl API
     curl_global_init(CURL_GLOBAL_ALL);
@@ -32,20 +48,47 @@ void sendFile(char* path, char* fileName) {
         curl_mime_name(field, "file");
         curl_mime_filedata(field, path);
 
-        // Set the URL that receives this POST and the associated form
+        // Set the URL and the associated form
         char* url = malloc(sizeof(char) * 80);
         strcpy(url, UPLOAD_URL);
         curl_easy_setopt(curl, CURLOPT_URL, strcat(url, fileName));
         curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
     
-        // Perform the request and check for errors
-        CURLcode res = curl_easy_perform(curl);
-        if(CURLE_OK != res) {
-            fprintf(stderr, "Request failed: %s\n", curl_easy_strerror(res));
-        }
+        performCurlRequest(curl, "\nUpload completed\n");
     
         curl_easy_cleanup(curl);    
         curl_mime_free(form);
+    }
+}
+
+void requestFile(char* path, bool write) {
+    // Init curl API
+    CURL* curl = curl_easy_init();
+    if (curl) {
+        // Set the URL
+        char* url = malloc(sizeof(char) * 80);
+        strcpy(url, DOWNLOAD_URL);
+        curl_easy_setopt(curl, CURLOPT_URL, strcat(url, path));
+
+        if (write) {
+            // Set the write function
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeData);
+
+            printf("%s", path);
+            FILE* file = fopen(path, "wb");
+            if (file) {
+                // Set the file that will receive the downloaded data
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+                
+                performCurlRequest(curl, "\nDownload complete\n");
+
+                fclose(file);
+            }
+        } else {
+            performCurlRequest(curl, "");
+        }
+    
+        curl_easy_cleanup(curl);
     }
 }
 
@@ -69,6 +112,26 @@ void upload() {
     sendFile(path, fileName);
 }
 
+void preview() {
+    printf("\nPreview\n");
+
+    char* path = malloc(sizeof(char));
+    printf("Entrez le chemin du fichier à afficher : ");
+    scanf("%s", path);
+
+    requestFile(path, false);
+}
+
+void download() {
+    printf("\nDownload\n");
+
+    char* path = malloc(sizeof(char));
+    printf("Entrez le chemin du fichier à télécharger : ");
+    scanf("%s", path);
+
+    requestFile(path, true);
+}
+
 int main() {
     printf("\nBienvenu sur OWNDRIVE\n");
     
@@ -80,12 +143,13 @@ int main() {
                 break;
             
             case 2:
-                printf("Preview\n");
+                preview();
                 break;
             
             case 3:
-                printf("Download\n");
+                download();
                 break;
         }
+        printf("\n");
     } while (0 != choice);
 }
